e172758290cd20fe3b7f4145257fb7a1b274b264
Thomas Bernard <thomas@ausha.co>
2023-08-29 22:34:48 +0200

    Toast shelf

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -4,13 +4,29 @@ import Button from '../Button';
 
 import styles from './ToastPlayground.module.css';
 import Toast from "../Toast";
+import ToastShelf from "../ToastShelf";
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState('');
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
-  const [isOpened, setIsOpened] = React.useState(false);
+  const [toasts, setToasts] = React.useState([]);
+
+  const popToast = () => {
+    setToasts(toasts => [...toasts, { message, variant, id: Math.random() }])
+    setMessage('');
+    setVariant(VARIANT_OPTIONS[0]);
+  }
+
+  const dismissToast = (toastId) => {
+    setToasts(toasts.filter(toast => toast.id !== toastId));
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    popToast();
+  }
 
   return (
     <div className={styles.wrapper}>
@@ -19,11 +35,9 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      {isOpened && (
-        <Toast variant={variant} handleDismiss={() => setIsOpened(false)}>{message}</Toast>
-      )}
+      <ToastShelf toasts={toasts} onDismiss={toastId => dismissToast(toastId)} />
 
-      <div className={styles.controlsWrapper}>
+      <form className={styles.controlsWrapper} onSubmit={onSubmit}>
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -66,10 +80,10 @@ function ToastPlayground() {
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <Button onClick={() => setIsOpened(true)}>Pop Toast!</Button>
+            <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
