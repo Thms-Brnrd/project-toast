6020ce40fc9db0f816eeeb2d2fc6ffe893884183
Thomas Bernard <thomas@ausha.co>
2023-08-29 23:18:29 +0200

    Extracting a custom hook

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import useEscapeKey from "../../hooks/useEscapeKey";
 
 export const ToastContext = React.createContext();
 function ToastProvider({ children }) {
   const [toasts, setToasts] = React.useState([]);
 
-  React.useEffect(() => {
-    function handleKeyDown(event) {
-      if (event.code === 'Escape') {
-        setToasts([]);
-      }
-    }
-
-    window.addEventListener('keydown', handleKeyDown);
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, []);
+  useEscapeKey(() => {
+    setToasts([]);
+  });
 
   const value = React.useMemo(() => ({
     toasts,
